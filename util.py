593e23a704ad2ce290cbae886a453a5b4ec02024
Mahesha999 <abnave.m@gmail.com>
2020-12-07 14:27:43 +0530

    updated with train and test set

--- util.py
+++ util.py
@@ -43,6 +43,54 @@ def load_preprocess_xy(split_percentage, scale_x, encode_y, dummify_y):
         return X_train, X_test, y_train, y_test
     
     return X, y
+
+def load_preprocess_xy_(file_path, split_percentage, scale_x, encode_y, dummify_y):
+    dataset = pd.read_csv(file_path)
+    X = dataset.iloc[:, 1:59].values
+    y = dataset.iloc[:, 59].values
+
+    if encode_y:
+        encoder = LabelEncoder()
+        y = encoder.fit_transform(y)
+
+    if scale_x:
+        sc_X = StandardScaler()
+        X = sc_X.fit_transform(X) #TODO should we scale test and train separately? Yes, but wont have much difference?
+
+    if dummify_y:
+        y = pd.get_dummies(y).values
+
+    if split_percentage > 0:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = split_percentage, random_state = 0)
+        return X_train, X_test, y_train, y_test
+    
+    return X, y
+
+# def load_preprocess_xy_(split_percentage, scale_x, encode_y, dummify_y):
+#     dataset_train = pd.read_csv("data/train.csv")
+#     dataset_test = pd.read_csv("data/test.csv")
+
+#     X_train = dataset_train.iloc[:, 1:59].values
+#     y_train = dataset_train.iloc[:, 59].values
+#     X_test = dataset_test.iloc[:, 1:59].values
+#     y_test = dataset_test.iloc[:, 59].values
+
+#     if encode_y:
+#         encoder = LabelEncoder()
+#         y = encoder.fit_transform(y)
+
+#     if scale_x:
+#         sc_X = StandardScaler()
+#         X = sc_X.fit_transform(X) #TODO should we scale test and train separately? Yes, but wont have much difference?
+
+#     if dummify_y:
+#         y = pd.get_dummies(y).values
+
+#     if split_percentage > 0:
+#         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = split_percentage, random_state = 0)
+#         return X_train, X_test, y_train, y_test
+    
+#     return X, y
     
 
 
