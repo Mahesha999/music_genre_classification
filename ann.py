5903fcf6cce8bdcc0a5739d0ad251788e08438d2
Mahesha999 <abnave.m@gmail.com>
2020-12-07 17:56:03 +0530

    Code cleanup

--- ann.py
+++ ann.py
@@ -12,12 +12,3 @@ def nn_fit_predict(X_train, y_train, X_test):
     model.compile(optimizer="Adam", loss='categorical_crossentropy', metrics=['accuracy'])
     model.fit(X_train, y_train, batch_size=10,epochs=100)
     return model.predict(X_test)
-  
-#   ANN_score=np.zeros(5,dtype=float)
-# i=0
-# for train_index,test_index in kf.split(X):
-#   X_train,X_test,y_train,y_test=X[train_index],X[test_index],Y[train_index],Y[test_index]
-#   ANN_score[i]=fn_ANN(X_train,X_test,y_train,y_test)
-#   i=i+1
-# avg_ANN_score=np.sum(ANN_score)/5.0
-# print(avg_ANN_score)
--- util.py
+++ util.py
@@ -5,44 +5,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
-
-# def load_scale_xy_with_25p_split():
-#     dataset = pd.read_csv("data/features_30_sec.csv")
-#     X = dataset.iloc[:, 1:59].values
-#     y = dataset.iloc[:, 59].values
-
-#     #importing the dataset
-#     X_train, X_test = train_test_split(X, test_size=0.25, random_state= 0)
-#     y_train, y_test = train_test_split(y, test_size=0.25, random_state= 0)
-
-#     #feature scaling
-#     sc_X = StandardScaler()
-#     X_train = sc_X.fit_transform(X_train)
-#     X_test = sc_X.transform(X_test)
-
-#     return X_train, X_test, y_train, y_test
-
-# def load_preprocess_xy(split_percentage, scale_x, encode_y, dummify_y):
-#     dataset = pd.read_csv("data/features_30_sec.csv")
-#     X = dataset.iloc[:, 1:59].values
-#     y = dataset.iloc[:, 59].values
-
-#     if encode_y:
-#         encoder = LabelEncoder()
-#         y = encoder.fit_transform(y)
-
-#     if scale_x:
-#         sc_X = StandardScaler()
-#         X = sc_X.fit_transform(X) #TODO should we scale test and train separately? Yes, but wont have much difference?
-
-#     if dummify_y:
-#         y = pd.get_dummies(y).values
-
-#     if split_percentage > 0:
-#         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = split_percentage, random_state = 0)
-#         return X_train, X_test, y_train, y_test
-    
-#     return X, y
+import seaborn as sns
 
 def load_preprocess_xy(file_path, split_percentage, scale_x, encode_y, dummify_y):
     dataset = pd.read_csv(file_path)
@@ -55,7 +18,7 @@ def load_preprocess_xy(file_path, split_percentage, scale_x, encode_y, dummify_y
 
     if scale_x:
         sc_X = StandardScaler()
-        X = sc_X.fit_transform(X) #TODO should we scale test and train separately? Yes, but wont have much difference?
+        X = sc_X.fit_transform(X)
 
     if dummify_y:
         y = pd.get_dummies(y).values
@@ -66,46 +29,6 @@ def load_preprocess_xy(file_path, split_percentage, scale_x, encode_y, dummify_y
     
     return X, y
 
-# def load_preprocess_xy_(split_percentage, scale_x, encode_y, dummify_y):
-#     dataset_train = pd.read_csv("data/train.csv")
-#     dataset_test = pd.read_csv("data/test.csv")
-
-#     X_train = dataset_train.iloc[:, 1:59].values
-#     y_train = dataset_train.iloc[:, 59].values
-#     X_test = dataset_test.iloc[:, 1:59].values
-#     y_test = dataset_test.iloc[:, 59].values
-
-#     if encode_y:
-#         encoder = LabelEncoder()
-#         y = encoder.fit_transform(y)
-
-#     if scale_x:
-#         sc_X = StandardScaler()
-#         X = sc_X.fit_transform(X) #TODO should we scale test and train separately? Yes, but wont have much difference?
-
-#     if dummify_y:
-#         y = pd.get_dummies(y).values
-
-#     if split_percentage > 0:
-#         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = split_percentage, random_state = 0)
-#         return X_train, X_test, y_train, y_test
-    
-#     return X, y
-    
-
-# def load_scale_x_encode_y():
-#     dataset = pd.read_csv("data/features_30_sec.csv")
-#     X = dataset.iloc[:, 1:59].values
-#     y = dataset.iloc[:, 59].values
-
-#     sc_X = StandardScaler()
-#     X = sc_X.fit_transform(X)
-
-#     encoder = LabelEncoder()
-#     y = encoder.fit_transform(y)
-#     return X, y
-
-    
 def get_accuracy(cm):
     sum = 0
     for i in range(cm.shape[0]):
@@ -143,3 +66,4 @@ def search_fit_predict_print(fit_predict_function, X_train, y_train, X_test, y_t
     print("Hyperparamters: ", ensemble.best_params_)
     plt.matshow(cm)
     plt.show()
+
